25d0e462e7b68b68f29d8a119ed45fa0c01f2ef9
Vidhyamahalakshmi <727723eucy062@skcet.ac.in>
2024-09-02 23:13:09 +0530

    Add files via upload

---
+++ Day2/day2cy.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+function Day2Cy1(){
+    return(
+        <div style={{backgroundColor:'gray', padding:'50px',margin:'10px',textAlign:'center'}}>
+            <h1 style={{color:'black'}}>Smile Component</h1>
+            <p>It is functional componenet</p>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb6gOxvbxL7xHqj6nZT_4PqOkt0r_2ri-yow&s"/>
+        </div>
+    )
+}
+export default Day2Cy1;
